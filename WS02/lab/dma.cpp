b1162d060e6d684cf72d165233c8332f9a3d1ca1
Ashar Afzal <ashara2030@gmail.com>
2025-09-26 10:24:14 -0400

    Updated to include code in namespace seneca

--- WS02/lab/dma.cpp
+++ WS02/lab/dma.cpp
@@ -1,47 +1,46 @@
 #include "dma.h"
 #include "cstr.h"         // For strlen, strcpy
 
-using namespace seneca;
-using namespace std;
+namespace seneca {
+	Samples* CreateSamples(const char* title) {
+		Samples* sample = new Samples;
+		sample->m_title = new char[strlen(title) + 1];
+		strcpy(sample->m_title, title);
+		sample->m_data = nullptr;
+		sample->m_size = 0;
+		return sample;
+	}
 
-Samples* CreateSamples(const char* title) {
-	Samples* sample = new Samples;
-	sample->m_title = new char[seneca::strlen(title) + 1];
-	seneca::strcpy(sample->m_title, title);
-	sample->m_data = nullptr;
-	sample->m_size = 0;
-	return sample;
-}
-
-void add(Samples& S, const int data[], int size) {
-	if (S.m_data == nullptr) {
-		S.m_data = new int[size];
-		for (int i = 0; i < size; i++) {
-			S.m_data[i] = data[i];
+	void add(Samples& S, const int data[], int size) {
+		if (S.m_data == nullptr) {
+			S.m_data = new int[size];
+			for (int i = 0; i < size; i++) {
+				S.m_data[i] = data[i];
+			}
+			S.m_size = size;
 		}
-		S.m_size = size;
+		else {
+			append(S.m_data, S.m_size, data, size);
+			S.m_size += size;
+		}
 	}
-	else {
-		append(S.m_data, S.m_size, data, size);
-		S.m_size += size;
-	}
-}
 
-void append(int*& data, int size, const int appendedData[], int dataSize) {
-	int* newData = new int[size + dataSize];
-	for (int i = 0; i < size; i++) {
-		newData[i] = data[i];
+	void append(int*& data, int size, const int appendedData[], int dataSize) {
+		int* newData = new int[size + dataSize];
+		for (int i = 0; i < size; i++) {
+			newData[i] = data[i];
+		}
+		for (int i = 0; i < dataSize; i++) {
+			newData[size + i] = appendedData[i];
+		}
+		delete[] data;
+		data = newData;
 	}
-	for (int i = 0; i < dataSize; i++) {
-		newData[size + i] = appendedData[i];
-	}
-	delete[] data;
-	data = newData;
-}
 
-void freemem(Samples*& s) {
-	delete[] s->m_title;
-	delete[] s->m_data;
-	delete s;
-	s = nullptr;
+	void freemem(Samples*& s) {
+		delete[] s->m_title;
+		delete[] s->m_data;
+		delete s;
+		s = nullptr;
+	}
 }
--- WS02/lab/dma.h
+++ WS02/lab/dma.h
@@ -1,19 +1,20 @@
 #ifndef DMA_H
 #define DMA_H
 
-struct Samples {
-    char* m_title;   // Dynamically allocated title
-    int* m_data;     // Dynamically allocated array of integers
-    int m_size;   // Number of elements in m_data
-};
+namespace seneca {
+    struct Samples {
+        char* m_title;   // Dynamically allocated title
+        int* m_data;     // Dynamically allocated array of integers
+        int m_size;   // Number of elements in m_data
+    };
 
-Samples* CreateSamples(const char* title);
+    Samples* CreateSamples(const char* title);
 
-void add(Samples& S, const int data[], int size);
+    void add(Samples& S, const int data[], int size);
 
-void append(int*& data, int size, const int appendedData[], int dataSize);
+    void append(int*& data, int size, const int appendedData[], int dataSize);
 
-void freemem(Samples*& s);
-
+    void freemem(Samples*& s);
+}
 
 #endif
