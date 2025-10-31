591a3f0ed8ff25623383a51540058dbab120ca26
Ashar Afzal <ashara2030@gmail.com>
2025-10-31 19:31:29 -0400

    Labs labs labs

--- WS04/lab/Account.cpp
+++ WS04/lab/Account.cpp
@@ -12,7 +12,8 @@
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
-//
+// Ashar Afzal     2025/10/07      Completing lab
+// Ashar Afzal	   2025/10/10      Implementing missing functions
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
 
@@ -86,6 +87,46 @@ namespace seneca {
       }
       return cout;
    }
+   Account::operator bool()const {
+       // An account is considered **valid** if it has a 5-digit integer account number, a non-negative balance, and a non-empty C-string for the account holder name.
+	   return isValidNumber(m_number) && m_balance >= 0 && m_holderName[0];
+   }
 
+   Account::operator int()const {
+	   // returns the account number
+	   return m_number;
+   }
+   Account::operator double()const {
+	   // Returns the **balance value**. This operator does not modify the **Account** object.
+       return m_balance;
+   }
+   Account::operator const char* () const {
+	   // Returns the address of the **account holder’s name**. This operator does not modify the **Account** object.
+	   return m_holderName;
+   }
+   char& Account::operator[](int index) {
+	   // Returns a reference to the character at the specified `index` in the account holder’s name. This operator allows modification of the character at that position. If the `index` is out of bounds, it should safely handle the request by looping back (e.g., `A[30]` should behave the same as `A[0]`).
+	   if (index < 0) {
+		   index = (index % NameMaxLen + NameMaxLen) % NameMaxLen;
+	   }
+	   else if (index >= NameMaxLen) {
+		   index = index % NameMaxLen;
+	   }
+	   return m_holderName[index];
+   }
+   // prefix
+   // Increases the account balance by 1 unit and returns the updated Account object.
+   
+   Account& Account::operator++() {
+	   m_balance += 1.0;
+	   return *this;
+   }
+   /* *Overload the** `+ = ` operator**to add a double value to an Account.This should act like depositing money into an Account(i.e., the value should be added to the balance).
+       If the Account is invalid or the value is negative, no action should be taken.
+       In any case, a reference to the** current object** (**Account**) should be returned. */
+   Account& Account::operator+=(double value) {
+	   m_balance += (value > 0 && *this) ? value : 0;
+	   return *this;
+   }
    
 }
--- WS04/lab/Account.h
+++ WS04/lab/Account.h
@@ -11,7 +11,8 @@
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
-//
+//Ashar Afzal	   2025/10/07      Completing lab
+//Ashar Afzal	   2025/10/10      Implementing missing functions
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
 #ifndef SENECA_ACCOUNT_H_
@@ -30,10 +31,26 @@ namespace seneca {
       Account(const char* holderName = nullptr);
       Account(const char* holderName, int number, double balance);
       std::ostream& display()const;
+      // state check, checks if account is valid or invalid
+	  operator bool()const;
+	  // int conversion operator, returns the account number
+	  operator int()const;
+      //  Double. Returns the **balance value**. This operator does not modify the **Account** object.
+	  operator double()const;
+      // char. Returns the address of the **account holder’s name**. This operator does not modify the **Account** object.
+	  operator const char* ()const;
+      // char& operator[](int index). Returns a reference to the character at the specified `index` in the account holder’s name. This operator allows modification of the character at that position. If the `index` is out of bounds, it should safely handle the request by looping back (e.g., `A[30]` should behave the same as `A[0]`).
+	  char& operator[](int index);
+      // sets the account to an empty state
+	  void setEmpty();
 
-
+      Account& operator=(double balance);
+      Account& operator=(int number);
+      Account& operator<<(Account&);
+	  Account& operator>>(Account&);
+	  Account& operator+=(double value);
+	  Account& operator-=(double value);
+	  Account& operator++(); // prefix
    };
-  
-
 }
 #endif // SENECA_ACCOUNT_H_
